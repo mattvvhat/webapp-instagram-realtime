a5c2ebd88e9b7b862ee2f0636106cc25201db10f
Matt Derasoirs <matt@planwork.us>
2014-03-25 22:33:34 -0400

    :yawn:

--- index.js
+++ index.js
@@ -1,4 +1,26 @@
-var app = require("express")();
+var url = require('url');
+var app = require('express')();
+var InstagramStream = require('instagram-realtime');
+var opts = require('./secrets.json');
+
+var stream = InstagramStream(app, opts);
+
+stream.on('unsubscribe', function (req, resp) {
+  console.log('unsubscribe'.green);
+  stream.subscribe({ tag : 'yolo' });
+});
+
+stream.on('new', function (req, body) {
+    console.log(body);
+});
+
+app.get('/', function (req, resp) {
+  resp.set('Content-Type', 'text/plain; charset=utf-8');
+  resp.end('🍕🏊');
+});
+
+stream.unsubscribe('all');
+
 
 app.get('/', function(req, res) {
   res.end('you know');
