8e82992766f7f90be81073174c480c8b163362ab
Matt Derasoirs <matt@planwork.us>
2014-03-28 01:16:44 -0400

    :seedling: Working on instagram-realtime@0.0.4
    TODO: Update for instagram-realtime@0.1.0

--- index.js
+++ index.js
@@ -1,6 +1,18 @@
-var app     = require('express')();
-var server  = require('http').createServer(app).listen(process.env.PORT || 5000);
-var io      = require('socket.io').listen(server, { log : false })
+var mongoose  = require('mongoose');
+var app       = require('express')();
+var server    = require('http').createServer(app).listen(process.env.PORT || 5000);
+var io        = require('socket.io').listen(server, { log : false })
+
+
+mongoose.connect('mongodb://nodejitsu_razorblade:doo8n92i8sb1eb7dpd3cekgoe3@ds029950.mongolab.com:29950/nodejitsu_razorblade_nodejitsudb5500477112');
+
+var Cat = mongoose.model('Cat', { name: String });
+
+var kitty = new Cat({ name: 'Zildjian' });
+kitty.save(function (err) {
+  if (err) // ...
+  console.log('meow');
+});
 
 var InstagramStream = require('instagram-realtime');
 var secrets = require('./secrets.json');
@@ -8,7 +20,8 @@ var secrets = require('./secrets.json');
 var opts = {
   client_id     : secrets.client_id,
   client_secret : secrets.client_secret,
-  url           : "http://instagram-realtime.herokuapp.com",
+  // Replace this with: callback_url: "http://irir.jit.su/callback"
+  url           : "http://irir.jit.su",
   callback_path : 'callback'
 };
 
@@ -19,7 +32,7 @@ var stream = InstagramStream(server, opts);
 stream.on('unsubscribe', function (req, resp) {
   console.log('unsubscribe');
   console.log(req.body);
-  stream.subscribe({ tag : 'yolo' });
+  stream.subscribe({ tag : 'video' });
 });
 stream.on('unsubscribe/error', function (error, req, resp) {
   console.log('unsubscribe/error');
@@ -37,7 +50,7 @@ stream.on('subscribe/error', function (error, req, resp) {
 
 stream.on('new', function (req, body) {
   console.log('new');
-  io.sockets.emit('new', JSON.stringify(req.body));
+  io.sockets.emit('new', req.body);
 });
 stream.on('new/error', function (error, req, resp) {
   console.log('new/error');
@@ -45,10 +58,6 @@ stream.on('new/error', function (error, req, resp) {
 });
 
 stream.unsubscribe('all');
-
-// socket.io
-
-
 
 // Express
 
